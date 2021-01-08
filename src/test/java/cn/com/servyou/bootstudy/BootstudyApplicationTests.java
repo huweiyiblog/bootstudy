efce1f82e88ea3ac3eee9c8849eac788ec02c4ea
huweiyi <1083397934@qq.com>
2021-01-08 09:26:42 +0800

    Initial commit

---
+++ src/test/java/cn/com/servyou/bootstudy/BootstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.servyou.bootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
